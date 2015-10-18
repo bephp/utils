9395fcaea93befae81e8bd61be4ee02c60d2d0e9
lloydzhou <lloydzhou@qq.com>
2015-10-18 19:27:07 +0800

    fixed bug

--- utils.php
+++ utils.php
@@ -69,7 +69,7 @@ function path() {
 function cache($key, $val=null, $expire=100) {
     static $_caches = null;
     static $_shm = null;
-    if ( null === $_shm ) $_shm = @shmop_open(ftok(config('mcache.solt', null, 'mcache.solt')),
+    if ( null === $_shm ) $_shm = @shmop_open(crc32(config('mcache.solt', null, 'mcache.solt')),
         'c', 0755, config('cache.size', null, 10485760));
     if ( null === $_caches && $_shm && ($size = intval(shmop_read($_shm, 0, 10))))
         $_caches = $size ? @unserialize(@shmop_read($_shm, 10, $size)) : array();
@@ -83,8 +83,11 @@ function cache($key, $val=null, $expire=100) {
 }
 function shmcache($key, $val=null, $expire=100) {
     static $_shm = null;
-    if ( null === $_shm ) $_shm = @shm_attach(ftok(config('mcache.solt', null, 'mcache.solt')),
-        config('cache.size', null, 10485760), 0755);
+    if ( null === $_shm ){
+        $_shm = @shm_attach(crc32(config('mcache.solt', null, 'mcache.solt')),
+            config('cache.size', null, 10485760), 0755);
+        register_shutdown_function(function() use ($_shm){ shm_detach($_shm); });
+    }
     if (($time = time()) && ($k = crc32($key)) && $val && $expire){
         shm_put_var($_shm, $k, array($time + $expire, $val));
         return $val;
